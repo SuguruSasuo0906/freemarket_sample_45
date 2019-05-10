ff9e69bef91ee8f4ea87ea842f1e130496c0f3b6
SuguruSasuo <fingertail0906@gmail.com>
2019-05-10 22:38:57 +0900

    create-toppage-footer

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,9 +3,8 @@ class Address < ApplicationRecord
     belongs_to :user
     has_many :prefectures
 
-    # validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
-    # validates :prefecture,presence: true
-    # validates :city,presence: true
-    # validates :block_number,presence: true
-    # validates :phone_number,presence: true,uniqueness: true, format: { with:  /\A0[5789]0[-(]?\d{4}[-)]?\d{4}\z/}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
+    validates :prefecture,presence: true
+    validates :city,presence: true
+    validates :block_number,presence: true
 end
--- app/models/phonenumber.rb
+++ app/models/phonenumber.rb
@@ -1,3 +1,5 @@
 class Phonenumber < ApplicationRecord
     belongs_to :user
+
+    validates :phone_number,presence: true,uniqueness: true, format: { with:  /\A0[5789]0[-(]?\d{4}[-)]?\d{4}\z/}
 end
