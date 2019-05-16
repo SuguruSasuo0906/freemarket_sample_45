f4743c26a033588422f0691137e6355ecfffd604
SuguruSasuo <fingertail0906@gmail.com>
2019-05-16 13:42:46 +0900

    create-sell-system vol1.01

--- app/assets/javascripts/dropzone.js
+++ app/assets/javascripts/dropzone.js
@@ -1,34 +1,9 @@
 $(function() {
+  // disable auto discover
   Dropzone.autoDiscover = false;
 
-  $("#image-dropzone").dropzone({
+  $(".dropzone").dropzone({
     maxFilesize: 1,
-    addRemoveLinks: true,
-    dictRemoveFile: "削除",
-    paramName: "image[image_url]",
-    success: function(file, response) {
-      $(file.previewElement)
-        .find(".dz-remove")
-        .attr("id", response.ImageId);
-      $(file.previewElement).addClass("dz-success");
-      console.log("OK");
-    },
-    removedfile: function(file) {
-      var id = $(file.previewTemplate)
-        .find(".dz-remove")
-        .attr("id");
-      $.ajax({
-        type: "DELETE",
-        url: "/" + locale + "/admin/images/" + id,
-        success: function(data) {
-          console.log("data.message");
-        }
-      });
-
-      var previewElement;
-      return (previewElement = file.previewElement) != null
-        ? previewElement.parentNode.removeChild(file.previewElement)
-        : void 0;
-    }
+    addRemoveLinks: true
   });
 });
