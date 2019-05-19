534b0c161d68866e59ce1ec5a4d458f8ad4ed575
SuguruSasuo <fingertail0906@gmail.com>
2019-05-19 15:10:43 +0900

    edit sell drop

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,22 @@
 class ItemsController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
-    def new
-        @item = Item.new
-    end
+  def new
+    @item = Item.new
+  end
 
-    def create
-        @Item = Item.new({image: params.require(:image)})
+  def create
+    @item = Item.new(item_params)
+    respond_to do |format|
+      if @item.save
+        format.json{}
+      end
     end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  end
 end
