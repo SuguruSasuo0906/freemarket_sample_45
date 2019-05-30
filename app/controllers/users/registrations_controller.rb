2613a0abe24f5e4aa2ca7d7b194cf621303eabb5
SuguruSasuo <fingertail0906@gmail.com>
2019-05-30 11:18:23 +0900

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   prepend_before_action :check_captcha, only: [:create]
   def new
-    @user=User.new
+    @user = User.new
   end
 
   def create
-    super
+    @user = User.new(user_params)
+    @user.save!
   end
 
   def after_sign_up_path_for(resource)
@@ -20,6 +21,10 @@ def check_captcha
   unless verify_recaptcha(model: resource)
     respond_with_navigational(resource) { render :new }
   end
+end
+
+def user_params
+  params.require(:user).permit(:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :year, :month, :day, :email, :password)
 end
 
 end
