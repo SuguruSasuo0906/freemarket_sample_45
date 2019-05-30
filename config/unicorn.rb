3eb148fbdd05b2205ee5b302a26db28445aabdd7
SuguruSasuo <fingertail0906@gmail.com>
2019-05-31 03:45:00 +0900

    Update unicorn.rb

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,11 +2,11 @@ app_path = File.expand_path('../../../', __FILE__)
 
 worker_processes 1
 
-working_directory "#{app_path}/freemarket_sample_45/current"
-listen "#{app_path}/freemarket_sample_45//shared/tmp/sockets/unicorn.sock"
-pid "#{app_path}/freemarket_sample_45//shared/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/freemarket_sample_45//shared/log/unicorn.stderr.log"
-stdout_path "#{app_path}/freemarket_sample_45//shared/log/unicorn.stdout.log"
+working_directory "#{app_path}/current"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
 listen 3000
 timeout 60
