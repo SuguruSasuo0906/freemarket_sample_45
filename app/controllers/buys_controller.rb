7a593de297b639d02051aed097c2eedca7fa8ef2
SuguruSasuo <fingertail0906@gmail.com>
2019-06-13 12:10:33 +0900

    create-buy-system
    商品購入画面完成

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,2 +1,11 @@
 class BuysController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+    @image = @item.images
+    @address = Address.find_by(user_id: current_user.id)
+    creditcard = Creditcard.find_by(user_id: current_user.id)
+    customer = Payjp::Customer.retrieve(creditcard.customer_id)
+    @card_information = customer.cards.retrieve(creditcard.card_id)
+  end
 end
