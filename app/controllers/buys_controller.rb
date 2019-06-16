c842e7aaf0f76ea8e9d498f153d369b1d21a65a2
SuguruSasuo <fingertail0906@gmail.com>
2019-06-16 15:30:09 +0900

    create-buy-system

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,23 +1,19 @@
 class BuysController < ApplicationController
+  before_action :set_item, only:[:index,:create]
+
 
   def index
-    @item = Item.find(params[:item_id])
-    @image = @item.images
-    @address = Address.find_by(user_id: current_user.id)
 
     Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    creditcard = Creditcard.find_by(user_id: current_user.id)
-    customer = Payjp::Customer.retrieve(creditcard.customer_id)
-    @card_information = customer.cards.retrieve(creditcard.card_id)
+    customer = Payjp::Customer.retrieve(current_user.creditcard.customer_id)
+    @card_information = customer.cards.retrieve(current_user.creditcard.card_id)
   end
 
   def create
-    @item = Item.find(params[:item_id])
-    creditcard = Creditcard.find_by(user_id: current_user.id)
 
     if @item.item_sold_id == 1
       Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-      customer_id = creditcard.customer_id
+      customer_id = current_user.creditcard.customer_id
       Payjp::Charge.create(currency:'jpy',amount: @item.price,customer: customer_id)
       
       Trade.create(item_id: @item.id, seller_id: @item.user_id, buyer_id: current_user.id)
@@ -27,6 +23,11 @@ class BuysController < ApplicationController
     else
       redirect_to item_buys_path
     end
+  end
 
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 end
