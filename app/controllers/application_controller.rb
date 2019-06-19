4bb324625aa1b2c92677b712e1d605e4af3179a1
SuguruSasuo <fingertail0906@gmail.com>
2019-06-19 22:06:16 +0900

    edit master

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production?
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :basic_auth, if: :production?
+  private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:year,:month,:day])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:nickname, :last_name,:irst_name, :last_name_kana, :first_name_kana, :year, :month, :day])
   end
-
-  private
 
   def production?
     Rails.env.production?
